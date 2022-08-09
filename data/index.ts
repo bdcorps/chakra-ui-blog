39261dfc4008048682dbaca070311bbb7134e5fb
Sukh <sunnyashiin@gmail.com>
2022-08-09 15:23:12 -0400

    add id to post

--- data/index.ts
+++ data/index.ts
@@ -7,7 +7,7 @@ export const sites: Site[] = [
     name: "Sukhpal Saini",
     description: "Full Stack Dev at XYZ",
     posts: [
-      { title: "Building a Subscription System using Stripe, Node.js, and MongoDB - Part 1: Recurring Payments", description: "We will create a subscription system that will let our users pay for a monthly plan using Stripe Checkout.", image: "https://google.com", link: "https://www.saasbase.dev/subscription-payments-1-adding-basic-and-pro-subscription-plans-using-stripe/" },
+      { id: 1, title: "Building a Subscription System using Stripe, Node.js, and MongoDB - Part 1: Recurring Payments", description: "We will create a subscription system that will let our users pay for a monthly plan using Stripe Checkout.", image: "https://google.com", link: "https://www.saasbase.dev/subscription-payments-1-adding-basic-and-pro-subscription-plans-using-stripe/" },
       { title: "Authentication System using Passport.js, Node.js, and MongoDB - Part 1: Google Login", description: "We will create an authentication system that will let our users log in using Google Sign-In.", image: "https://google.com", link: "https://www.saasbase.dev/building-an-authentication-system-using-passport-js-node-js-and-mongodb-part-1-google-login/" }
     ]
   }
@@ -17,8 +17,8 @@ export const sites: Site[] = [
     name: "ashi",
     description: "ashi's website",
     posts: [
-      { title: "ashi are cool", description: "coolio", image: "https://google.com", link: "" },
-      { title: "people ashi are cool", description: "coolio", image: "https://google.com", link: "" }
+      { id: 2, title: "ashi are cool", description: "coolio", image: "https://google.com", link: "" },
+      { id: 3, title: "people ashi are cool", description: "coolio", image: "https://google.com", link: "" }
     ]
   }
 ]
